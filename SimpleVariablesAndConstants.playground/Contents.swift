e47091ad463e1344bc8bcec650d07abb4a0f9980
Marc De Jesus <marcdejesusdev@gmail.com>
2025-05-18 18:19:51 -0400

    Exercise 1

---
+++ SimpleVariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,9 @@
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day) and the temperature is \(dailyTemperature) degrees")
+var temperature = 70
+print("The temperature on \(day) morning is \(temperature) degrees")
+temperature = 80
+print("The temperature on \(day) evening is \(temperature) degrees")
+let weeklyTemperature = 75
+print("The average temperature for the week is \(weeklyTemperature) degrees")
